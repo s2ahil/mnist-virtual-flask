e795d34208d81dc94302a7b2ea4eddbb217885bd
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-08-19 20:33:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,35 +5,35 @@ import numpy as np
 import keras
 app = Flask(__name__)
 
-model = joblib.load('mnist.pkl')
+# model = joblib.load('mnist.pkl')
 
 @app.route('/')
 def hello_world():
     # Read the uploaded image
- image_path = '4.png'  # Replace with the path to your image
+#  image_path = '4.png'  # Replace with the path to your image
 
 
 
- def preprocess():
-  gray_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+#  def preprocess():
+#   gray_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-# Resize the image to 28x28 pixels (MNIST size)
-  resized_image = cv2.resize(gray_image, (28, 28))
+# # Resize the image to 28x28 pixels (MNIST size)
+#   resized_image = cv2.resize(gray_image, (28, 28))
 
-# Invert the colors (black to white, white to black)
-  inverted_image = cv2.bitwise_not(resized_image)
+# # Invert the colors (black to white, white to black)
+#   inverted_image = cv2.bitwise_not(resized_image)
 
 
-  return inverted_image
+#   return inverted_image
  
 
- pre_img=preprocess()
- prediction = model.predict(np.array([pre_img]))
- predicted_digit = np.argmax(prediction)
+#  pre_img=preprocess()
+#  prediction = model.predict(np.array([pre_img]))
+#  predicted_digit = np.argmax(prediction)
 
- print(predicted_digit)
+#  print(predicted_digit)
  
- return 'Hello, World BRO! hh  d'+str(predicted_digit)
+ return 'Hello, World BRO! hh  d'
 
 
 if __name__ == '__main__':
