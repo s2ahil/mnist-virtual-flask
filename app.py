2f52fd418d373c10c38899bd2755125a4c18acb4
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-08-19 23:03:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,21 @@ loaded_model = model_from_json(loaded_model_json)
 
 loaded_model.load_weights("models_w.h5")
 
+image_path = '4.png'  # Replace with the path to your image
+
+
+def preprocess():
+ gray_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+# Resize the image to 28x28 pixels (MNIST size)
+ resized_image = cv2.resize(gray_image, (28, 28))
+
+# Invert the colors (black to white, white to black)
+ inverted_image = cv2.bitwise_not(resized_image)
+
+
+ return inverted_image
+
 
 app = Flask(__name__)
 
@@ -40,13 +55,12 @@ def hello_world():
 #   return inverted_image
  
 
-#  pre_img=preprocess()
-#  prediction = model.predict(np.array([pre_img]))
-#  predicted_digit = np.argmax(prediction)
-
-#  print(predicted_digit)
+ pre_img=preprocess()
+ prediction = model.predict(np.array([pre_img]))
+ predicted_digit = np.argmax(prediction)
+ print(predicted_digit)
  
- return 'Hello, World BRO! hh  d'
+ return 'Hello, World BRO! hh  d'+str(predicted_digit)
 
 
 if __name__ == '__main__':
