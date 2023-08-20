e49e4b92d82afe9b668e1d16299b4b6cf6f52069
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-08-20 12:21:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,7 +133,24 @@ async def index():
 @app.post("/process-canvas-image/")
 async def process_canvas_image(request_data: dict):
     dataURI = request_data.get("dataURI")
-    return {"predicted_digit": str(dataURI)}
+    encoded_data = dataURI.split(",")[1]
+    decoded_image = base64.b64decode(encoded_data)
+
+            # Convert image data to OpenCV format
+    image_array = np.array(PIL.Image.open(io.BytesIO(decoded_image)))
+
+            # Preprocess the image
+    gray_image = cv2.cvtColor(image_array, cv2.COLOR_RGB2GRAY)
+    resized_image = cv2.resize(gray_image, (28, 28))
+    inverted_image = cv2.bitwise_not(resized_image)
+    pre_img = inverted_image.reshape(1, 28, 28, 1).astype('float32') / 255
+
+            # Predict digit using loaded model
+    prediction = loaded_model.predict(pre_img)
+    predicted_digit = np.argmax(prediction)
+
+    return {"predicted_digit": int(predicted_digit)}
+    # return {"predicted_digit": str(dataURI)}
     # try:
     #     dataURI = request_data.get("dataURI")
         
